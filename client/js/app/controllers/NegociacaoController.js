d28b4c55a8fed3c9d97da062ad4d03d483a9257e
Geovane Menezes <geovane.menezes@seati.ma.gov.br>
2019-12-02 13:08:27 -0300

    Commit Inicial

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,13 @@
+class NegociacaoController {
+
+  adiciona(event) {
+    event.preventDefault();
+
+    alert(json);
+  }
+}
+
+// '12-10-2016' -> new Date(2016, 10, 12);
+//
+// var sData = '12-10-2016'.split('-');
+// var data = new Date(sData[2])
